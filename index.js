fa1fe87d7df01b972c8ec083d35464f50f16adaf
Nathan <nathan@users.noreply.github.com>
2017-02-26 05:00:58 -0500

    Export getOutputName for compatibility

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ exports.loadBook = require('./lib/load')
 exports.createArchive = require('./lib/create')
 exports.ensureUUID = require('./lib/ensure')
 exports.generate = require('./lib/generate')
+exports.getOutputName = require('./lib/format').outputName
